2f64bc1eedbbc962c645e4db4467d8584827e916
Code-xed <ngudulu@gmail.com>
2021-09-23 00:02:34 +0530

    Update Sample_Config.py

--- Sample_Config.py
+++ Sample_Config.py
@@ -1,6 +1,5 @@
 import os
 
-class Config(object):
-    api_id = int(os.environ.get("API_ID"))
-    api_hash = os.environ.get("API_HASH")
-    session_string = os.environ.get("SS")
+api_id = int(os.environ.get("API_ID"))
+api_hash = os.environ.get("API_HASH")
+session_string = os.environ.get("SS")
