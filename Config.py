da8ee2ad1d7b72e3744aa6b9c088c0fe4019e92c
Code-xed <ngudulu@gmail.com>
2021-09-22 22:23:28 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,4 +1,6 @@
 import os
-api_id = int(os.environ.get("API_ID"))
-api_hash = os.environ.get("API_HASH")
-session_string = os.environ.get("SS")
+
+class Config:
+    api_id = int(os.environ.get("API_ID"))
+    api_hash = os.environ.get("API_HASH")
+    session_string = os.environ.get("SS")
