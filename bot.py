d54ebd4b2bf2d814e8bd2f21813274c4007c62c1
Code-xed <ngudulu@gmail.com>
2021-09-23 00:49:32 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,5 +5,7 @@ with open("config.ini", "w") as f:
     f.write(f"[pyrogram]\napi_id = {api_id}\napi_hash = {api_hash}")
 
 app = Client(session_string)
+app.start()
 print("Successfully Logged In")
 app.send_message("me","Working")
+app.close()
