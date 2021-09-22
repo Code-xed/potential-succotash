480dcf0f86041150e9d7d73abef42dcd9415f274
Code-xed <ngudulu@gmail.com>
2021-09-23 00:01:29 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -3,9 +3,9 @@ from Sample_Config import Config
 
 class Development(Config):
     # get this values from the my.telegram.org
-    APP_ID = 6
-    API_HASH = "eb06d4abfb49dc3eeb1aeb98ae0f581e"
+    app_id = 6
+    api_hash = "eb06d4abfb49dc3eeb1aeb98ae0f581e"
     # After cloning the repo and installing requirements do python3 telesetup.py an fill that value with this
-    STRING_SESSION = "Your value"
+    string_session = "Your value"
     # create a new bot in @botfather and fill the following vales with bottoken
-    TG_BOT_TOKEN = "Your value"
+    tg_bot_token = "Your value"
